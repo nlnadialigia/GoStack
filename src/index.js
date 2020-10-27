ff4b2bb7c5f9fba4362b8c4fe33685b86e8b0aab
nlnadialia <nlnadialigia@gmail.com>
2020-10-26 23:16:39 -0300

    Aplicação Funcional

--- src/index.js
+++ src/index.js
@@ -1,9 +1,66 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (request, response) =>{
-  return response.json({message: 'Hello World!'})
+const projects = []
+
+app.get('/projects', (request, response) =>{
+  const { title, owner } = request.query
+
+  const results = title
+  ? projects.filter(project => project.title.includes(title))
+  : projects
+  
+  return response.json(results)
 })
 
-app.listen(3333, console.log('SERVER IS RUNNING'))
+app.post('/projects', (request, response) => {
+  const {title, owner} = request.body
+
+  const projetct = { id:uuid(), title, owner }
+
+  projects.push(projetct)
+
+  return response.json(projetct)
+})
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params
+  const {title, owner} = request.body
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project
+
+  return response.json(project)
+})
+
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
+})
+
+app.listen(3333, () => {
+  console.log('🚀Back-end started');
+})
