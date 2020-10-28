41e599f9ac66e93750e605249501831344505abd
nlnadialia <nlnadialigia@gmail.com>
2020-10-28 05:35:38 -0300

    update Propriedades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,19 @@ import Header from './components/Header'
 function App() {
   return (
     <>
-      < Header />
-      < Header />
+      <Header title='HomePage'>
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title='Projects'>
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
     )
 }
