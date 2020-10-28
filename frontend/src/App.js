f41b4cdf771adb6d30781b5e583af413d9486d37
nlnadialia <nlnadialigia@gmail.com>
2020-10-28 06:26:31 -0300

    update Importantdo CSS e imagens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useState }from 'react'
 
+import './App.css'
+import backgroundImage from './assets/background.jpeg'
 import Header from './components/Header'
 
+
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+
+  function handleAddProject() {
+    // projects.push(`Novo projeto ${Date.now()}`)
+
+    setProjects([... projects, `Novo Projeto ${Date.now()}`])
+    console.log(projects);
+  }
+
   return (
     <>
-      <Header title='HomePage'>
+      <Header title='Projects' />
+        <img width={100} src={backgroundImage} alt="Nature"/>
         <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
+          {projects.map(project => <li key={project}>{project}</li> )}
         </ul>
-      </Header>
-      <Header title='Projects'>
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+
+        <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
     </>
     )
 }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -17,6 +17,19 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i, //i => case insensitive
+        use: 
+          { loader: 'file-loader' },
       }
     ]
   }
