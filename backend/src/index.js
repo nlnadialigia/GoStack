7420359f4a0ab23612078215e1a2642ee6d813e3
Nadia Ligia <nlnadialigia@gmail.com>
2020-10-28 09:14:10 -0300

    Listando Projetos da API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const { v4: uuidv4, validate: isUuid } = require('uuid')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 const projects = []
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import React, { useState }from 'react'
+import React, { useEffect, useState }from 'react'
+import api from './services/api'
 
 import './App.css'
-import backgroundImage from './assets/background.jpeg'
 import Header from './components/Header'
 
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
 
   function handleAddProject() {
     // projects.push(`Novo projeto ${Date.now()}`)
@@ -18,9 +24,8 @@ function App() {
   return (
     <>
       <Header title='Projects' />
-        <img width={100} src={backgroundImage} alt="Nature"/>
         <ul>
-          {projects.map(project => <li key={project}>{project}</li> )}
+          {projects.map(project => <li key={project.id}>{project.title}</li> )}
         </ul>
 
         <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
