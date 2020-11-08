bbef3590fa9f1114ad50b9a8656b2a90257250bd
Nadia <nlnadialigia@gmail.com>
2020-11-08 17:03:28 -0300

    pictures upload

--- GoStack-N2/iniciando-back-end/src/routes/users.routes.ts
+++ GoStack-N2/iniciando-back-end/src/routes/users.routes.ts
@@ -1,19 +1,24 @@
 import { Router } from 'express';
-import CreateUserService from '../services/CreateUserService'
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
-    const { name, email, password } = request.body
+    const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService()
+    const createUser = new CreateUserService();
 
     const user = await createUser.execute({
       name,
       email,
-      password
-    })
+      password,
+    });
 
     const userWithoutPassword = {
       id: user.id,
@@ -21,14 +26,21 @@ usersRouter.post('/', async (request, response) => {
       email: user.email,
       created_at: user.created_at,
       updated_at: user.updated_at,
-    }
+    };
 
-    return response.json(userWithoutPassword)
-
+    return response.json(userWithoutPassword);
   } catch (err) {
-    return response.status(400).json({ error: err.message })
+    return response.status(400).json({ error: err.message });
   }
 });
 
-
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+    return response.json({ ok: true });
+  },
+);
 export default usersRouter;
