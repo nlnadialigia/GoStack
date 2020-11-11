ae2555b12370d96d8fbc378f4d37c82e4d7e8c0a
Nadia Ligia <nlnadialigia@gmail.com>
2020-11-11 17:24:36 -0300

    tratativa de erros

--- GoStack-N3/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ GoStack-N3/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -2,7 +2,9 @@ import React, { FormEvent, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './style';
+import {
+  Title, Form, Repositories, Error,
+} from './style';
 
 interface Repository{
   full_name: string
@@ -14,28 +16,42 @@ interface Repository{
 }
 
 const Dashboard: React.FC = () => {
+  const [inputError, setInputError] = useState('');
   const [newRepo, setNewRepo] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function hadleAddRepository(event: FormEvent<HTMLElement>) {
     event.preventDefault();
 
-    const response = await api.get<Repository>(`repos/${newRepo}`);
+    if (!newRepo) {
+      setInputError('Digite o autor/repositório');
+      return;
+    }
 
-    const repository = response.data;
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
 
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch {
+      setInputError('Repositório não encontrado.');
+    }
   }
 
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Explore repositórios no GitHub</Title>
-      <Form onSubmit={hadleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={hadleAddRepository}>
         <input value={newRepo} onChange={(e) => setNewRepo(e.target.value)} type="text" placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      { inputError && <Error>{inputError}</Error> }
+
       <Repositories>
         {repositories.map((repository) => (
           <a key={repository.full_name} href="teste">
--- GoStack-N3/primeiro-projeto-react/src/pages/Dashboard/style.ts
+++ GoStack-N3/primeiro-projeto-react/src/pages/Dashboard/style.ts
@@ -1,5 +1,9 @@
 import { shade } from 'polished';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface FormProps{
+  hasError: boolean
+}
 
 export const Title = styled.h1`
   max-width: 450px;
@@ -9,7 +13,7 @@ export const Title = styled.h1`
   margin-top: 40px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 714px;
   
@@ -21,12 +25,18 @@ export const Form = styled.form`
     padding: 0 24px;
     border:0;
     border-radius: 5px 0 0 5px;
+    border: 3px solid #FFFFFF;
+    border-right: 0;
+
+    ${(props) => props.hasError && css`
+    border-color: #C53030;
+    `}
 
     &::placeholder{
       color: #A8A8B3;
     }
   }
-
+  
   button{
     width: 210px;
     height: 70px;
@@ -41,6 +51,13 @@ export const Form = styled.form`
      background: ${shade(0.2, '#04D361')} 
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #C53030;
+  margin-top: 8px;
+  font-weight: bold;
 `;
 
 export const Repositories = styled.div`
