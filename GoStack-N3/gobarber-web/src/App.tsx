22d120c7469177ee20d4d2e61eb18e121caf5237
Nadia Ligia <nlnadialigia@gmail.com>
2020-11-16 23:58:23 -0300

    localStorage

--- GoStack-N3/gobarber-web/src/App.tsx
+++ GoStack-N3/gobarber-web/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 import SignIn from './pages/Signin/index';
 // import SignUp from './pages/Signup/index';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
--- GoStack-N3/gobarber-web/src/pages/Signin/index.tsx
+++ GoStack-N3/gobarber-web/src/pages/Signin/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
@@ -9,7 +9,7 @@ import { Container, Content, Background } from './style';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import getValidationErrors from '../../utils/getValidationErrors';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 
 interface SignInFormData {
   email: string;
@@ -18,7 +18,7 @@ interface SignInFormData {
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  const { signIn } = useContext(AuthContext);
+  const { signIn } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
