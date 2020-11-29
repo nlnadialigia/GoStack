56b037d7dabbab06c8c3574372a527c5e6c99f10
Nadia Ligia <nlnadialigia@gmail.com>
2020-11-29 00:55:47 -0300

    Autenticação e cadastro

---
+++ GoStack-N3/appgobarber/src/utils/getValidationErrors.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { ValidationError } from 'yup';
+
+interface Errors {
+  [key: string]: string;
+}
+
+export default function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
+
+  err.inner.forEach(error => {
+    validationErrors[error.path] = error.message;
+  });
+
+  return validationErrors;
+}
