3b6af722ab735f2737dd299682c315bfab384187
Nadia Ligia <nlnadialigia@gmail.com>
2020-11-30 23:39:53 -0300

    Dependency Inversion Principle

--- GoStack-N4/gobarber-api/src/modules/appointments/infra/http/appointments.routes.ts
+++ GoStack-N4/gobarber-api/src/modules/appointments/infra/http/appointments.routes.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '@shared/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository()
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
   const parsedDate = parseISO(date);
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(appointmentsRepository);
 
   const appointment = await createAppointment.execute({
     provider_id,
@@ -22,10 +22,9 @@ appointmentsRouter.post('/', async (request, response) => {
   return response.json(appointment);
 });
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 export default appointmentsRouter;
--- GoStack-N4/gobarber-api/src/modules/appointments/services/CreateAppointmentService.ts
+++ GoStack-N4/gobarber-api/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,22 +1,23 @@
 /* eslint-disable class-methods-use-this */
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import Appointment from '@modules/appointments/infra/entities/Appointment';
-import AppointmentsRepository from '../../../shared/infra/typeorm/repositories/AppointmentsRepository';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
-interface Request {
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentSevices {
-  public async execute({ provider_id, date }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  constructor(
+    private appointmentsRepository: IAppointmentsRepository
+  ) { }
 
+  public async execute({ provider_id, date }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -24,12 +25,12 @@ class CreateAppointmentSevices {
       throw new AppError('This appointment is already booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
 
-    await appointmentsRepository.save(appointment);
+    await this.appointmentsRepository.save(appointment);
 
     return appointment;
   }
